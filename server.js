74c890231c3451d6cae71dcdb809e66d4111ade1
unknown <snehagurav14.sg@gmail.com>
2020-10-21 23:05:52 +0530

    fprt

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-const express = require("express")
-const app = express()
-const PORT = 4000
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
 const dotenv = require("dotenv");
 dotenv.config();
-require("./db")
+require("./db");
 app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+app.use(express.static(path.join(__dirname, "build")));
+console.log(path.__dirname);
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-const userRoute = require("./routes/userRoute")
-app.use(userRoute)
+const userRoute = require("./routes/userRoute");
+app.use(userRoute);
 
-
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
-app.listen(PORT,()=>{
-console.log("server started")
-})
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+app.listen(PORT, () => {
+    console.log("server started");
+});
