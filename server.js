ffbe577adec700a874b5ae0dde7a8c5914c1fe02
unknown <snehagurav14.sg@gmail.com>
2020-10-21 23:16:59 +0530

    m

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) => {
-    res.send("hello");
-});
-console.log(path.__dirname);
+
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
